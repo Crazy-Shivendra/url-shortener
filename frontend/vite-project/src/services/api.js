ae3cf536bac0e1dc8c8dcd2ef83db451b0fbcba6
Shivendra Sudhanshu <sshivendra2004@gmail.com>
2026-04-28 10:45:28 +0530

    frontend ready for deploy

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const BASE_URL = "http://localhost:3000"; // later env me daalenge
+const BASE_URL = "https://url-shortener-1-dms6.onrender.com"; // later env me daalenge
 
 // 🔹 1. Shorten URL
 export const shortenUrl = async (url, customCode) => {
